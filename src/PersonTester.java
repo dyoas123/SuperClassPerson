05acb7debb6c390d07cf88eb90c9f5f2d50774c7
dyoas123 <rautrautjoash1@gmail.com>
2023-11-22 20:58:44 +0800

    added persontester

---
+++ src/PersonTester.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rautr
+ */
+public class PersonTester {
+    
+}
+public static void main(String[] args)
+  {
+ Person a = new Person("Anil", 1992);
+    Student b = new Student("Jimmy", 1919, "Information Technology");
+    Instructor c = new Instructor("Mike", 1998, 95000);
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+}
+}
